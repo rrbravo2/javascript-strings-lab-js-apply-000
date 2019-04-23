05e2895d94fb657fc6c10fe2b5e72e4b26ed6fde
Ryan Bravo <rrbravo2@gmail.com>
2019-04-23 02:36:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "Hello, everybody!";
+var greeting = "";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello," + specialGuest + "Neil deGrasse Tyson!";
+var greetSpecialGuest = "" + specialGuest + "";
 
 var topic = "space";
 
